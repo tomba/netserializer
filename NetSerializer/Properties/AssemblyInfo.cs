a0e64454ce2142898023417f7834ac07bb27523f
Tomi Valkeinen <tomba@iki.fi>
2012-03-19 17:08:24 +0200

    fix assembly copyright

--- NetSerializer/Properties/AssemblyInfo.cs
+++ NetSerializer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetSerializer")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Tomi Valkeinen 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
