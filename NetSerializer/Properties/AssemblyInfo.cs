ddfdde3da8d22a7607c76fdbdab124e9363e26fd
Tomi Valkeinen <tomba@iki.fi>
2013-03-18 13:46:11 +0200

    update assembly information

--- NetSerializer/Properties/AssemblyInfo.cs
+++ NetSerializer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NetSerializer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A very fast and minimal serializer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tomi Valkeinen")]
 [assembly: AssemblyProduct("NetSerializer")]
-[assembly: AssemblyCopyright("Copyright © Tomi Valkeinen 2012")]
+[assembly: AssemblyCopyright("Copyright © 2012 Tomi Valkeinen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
