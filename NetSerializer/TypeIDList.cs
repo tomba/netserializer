2c1ccbaa87793d3547afa8d129d96a9fd5a2881d
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2015-09-13 23:00:41 +0300

    Add TypeIDList

---
+++ NetSerializer/TypeIDList.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Copyright 2015 Tomi Valkeinen
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+
+namespace NetSerializer
+{
+	/// <summary>
+	/// Threadsafe TypeID -> TypeData list, which supports lockless reading.
+	/// </summary>
+	class TypeIDList
+	{
+		TypeData[] m_array;
+		object m_writeLock = new object();
+
+		const int InitialLength = 256;
+
+		public TypeIDList()
+		{
+			m_array = new TypeData[InitialLength];
+		}
+
+		public bool ContainsTypeID(uint typeID)
+		{
+			return typeID < m_array.Length && m_array[typeID] != null;
+		}
+
+		public TypeData this[uint idx]
+		{
+			get
+			{
+				return m_array[idx];
+			}
+
+			set
+			{
+				lock (m_writeLock)
+				{
+					Debug.Assert(m_array[idx] == null);
+
+					if (idx >= m_array.Length)
+					{
+						var newArray = new TypeData[NextPowOf2(idx + 1)];
+						Array.Copy(m_array, newArray, m_array.Length);
+						m_array = newArray;
+					}
+
+					m_array[idx] = value;
+				}
+			}
+		}
+
+		uint NextPowOf2(uint v)
+		{
+			v--;
+			v |= v >> 1;
+			v |= v >> 2;
+			v |= v >> 4;
+			v |= v >> 8;
+			v |= v >> 16;
+			v++;
+			return v;
+		}
+	}
+}
