d3cee77538fdd42f54d6f0ea1674ee0a20077fcc
jkuehner <jochen.kuehner@kardex.com>
2015-11-14 12:47:04 +0100

    Version Serializer

--- NetSerializer/Serializer.cs
+++ NetSerializer/Serializer.cs
@@ -24,6 +24,7 @@ namespace NetSerializer
 		readonly static ITypeSerializer[] s_typeSerializers = new ITypeSerializer[] {
 			new ObjectSerializer(),
 			new PrimitivesSerializer(),
+			new VersionSerializer(),
 			new ArraySerializer(),
 			new EnumSerializer(),
 			new DictionarySerializer(),
